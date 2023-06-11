345ce822ccb7a8368c534aecceaa67e32adf55b1
614uc0 <glaucomv@gmail.com>
2023-06-11 11:32:50 -0300

    preparando para colocar dados na tela

--- src/main/java/devandroid/glaucomv/apptesteparalistavip/model/Pessoa.java
+++ src/main/java/devandroid/glaucomv/apptesteparalistavip/model/Pessoa.java
@@ -1,4 +1,61 @@
 package devandroid.glaucomv.apptesteparalistavip.model;
 
+import androidx.annotation.NonNull;
+
 public class Pessoa {
+
+    // Construtor
+    // Atributos
+    // Métodos de acesso - Getter and Setter
+
+    private String nome;
+    private String sobrenome;
+    private String parentesco;
+    private String telefone;
+
+    public Pessoa(){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getParentesco() {
+        return parentesco;
+    }
+
+    public void setParentesco(String parentesco) {
+        this.parentesco = parentesco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                ", parentesco='" + parentesco + '\'' +
+                ", telefone='" + telefone + '\'' +
+                '}';
+    }
 }
--- src/main/java/devandroid/glaucomv/apptesteparalistavip/view/MainActivity.java
+++ src/main/java/devandroid/glaucomv/apptesteparalistavip/view/MainActivity.java
@@ -3,14 +3,59 @@ package devandroid.glaucomv.apptesteparalistavip.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import devandroid.glaucomv.apptesteparalistavip.R;
+import devandroid.glaucomv.apptesteparalistavip.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
+
+    Pessoa pessoa;
+
+    String dadosPessoa;
+
+    EditText editTextNome;
+    EditText editTextSobrenome;
+    EditText editTextParentesco;
+    EditText editTextTelefone;
+
+    Button buttonLimpar;
+    Button buttonSalvar;
+    Button buttonFinalizar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pessoa = new Pessoa();
+        pessoa.setNome("Marco");
+        pessoa.setSobrenome("Maddo");
+        pessoa.setParentesco("Pai");
+        pessoa.setTelefone("21985655847");
+
+        editTextNome = findViewById(R.id.editTextNome);
+        editTextSobrenome = findViewById(R.id.editTextSobrenome);
+        editTextParentesco = findViewById(R.id.editTextParentesco);
+        editTextTelefone = findViewById(R.id.editTextTelefone);
+
+        buttonLimpar = findViewById(R.id.buttonLimpar);
+        buttonSalvar = findViewById(R.id.buttonSalvar);
+        buttonFinalizar = findViewById(R.id.buttonFinalizar);
+
+        editTextNome.setText(pessoa.getNome());
+        editTextSobrenome.setText(pessoa.getSobrenome());
+        editTextParentesco.setText(pessoa.getParentesco());
+        editTextTelefone.setText(pessoa.getTelefone());
+
+        dadosPessoa = pessoa.getNome();
+        dadosPessoa = pessoa.getSobrenome();
+        dadosPessoa = pessoa.getParentesco();
+        dadosPessoa = pessoa.getTelefone();
+
+        Log.i("POOAndroid",pessoa.toString());
+
     }
 }
